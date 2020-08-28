3dccc8e5bf128d6ef8ad3fd98e810e68f38e9df7
Alberto Young <alyoung1991@gmail.com>
2020-08-28 10:28:22 -0400

    Added age verification and products page

---
+++ ageVerify.js
@@ -0,0 +1,7 @@
+$('.modal').modal('show');
+
+function displayUnderageError(){
+	var modalBody = document.querySelector(".modal-body");
+	modalBody.innerHTML = "We're sorry, but you are not permitted to access this site.";
+	modalBody.style.color = "red";
+}
